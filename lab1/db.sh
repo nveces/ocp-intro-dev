01b1ada863c0d89f7551cd257723d698d3e85f2d
splatas <splatas@redhat.com>
2025-04-11 16:41:04 +0200

    Cambio de imagen. Agrego scripts para desplegar DB PostgreSql

---
+++ lab1/db.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+printf "\n\n######## Deploying PostgreSQL ########\n"
+
+# kubectl apply -f src/main/kubernetes/pgsql.yml
+oc apply -f pgsql.yml
+oc wait --for=condition=available --timeout=60s deployment/postgresql
+
+printf "\n\n######## Create Schema PostgreSQL ########\n"
+
+# kubectl apply -f src/main/kubernetes/pgsql-db-creator.yml
+oc apply -f pgsql-db-creator.yml
+oc wait --for=condition=complete job/petclinic-schema
