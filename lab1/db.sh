cf8b25f0ad127bb0236682bcc5b149ae847a733a
splatas <splatas@gmail.com>
2025-04-14 16:52:33 +0200

    Actualicé las referencias para la creación de la DB

--- lab1/db.sh
+++ lab1/db.sh
@@ -4,7 +4,8 @@ printf "\n\n######## Deploying PostgreSQL ########\n"
 
 # kubectl apply -f src/main/kubernetes/pgsql.yml
 oc apply -f pgsql.yml
-oc wait --for=condition=available --timeout=60s deployment/postgresql
+# oc wait --for=condition=available --timeout=60s deployment/postgresql
+oc wait --for=condition=available --timeout=60s deployment/petclinic-database
 
 printf "\n\n######## Create Schema PostgreSQL ########\n"
 
